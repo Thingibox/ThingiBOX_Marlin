e82a03abedd2b924261321820d0aa538c9566d5e
Iván Fernández <ifernandez@redsauce.net>
2024-07-25 10:48:09 +0200

    Fixed the bed size for the ION v7 XL

--- config/IONv7XL/Configuration.h
+++ config/IONv7XL/Configuration.h
@@ -1574,7 +1574,7 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 200
+#define X_BED_SIZE 300
 #define Y_BED_SIZE 200
 
 // Travel limits after homing (units are in mm)
